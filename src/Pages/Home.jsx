039146e7b298b71beea21de4b88dfb3249f9e0ad
Manishshriwastva <Manishshriwastva@gmail.com>
2024-06-09 18:07:59 +0530

    init

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Layoutt from '../Layout/Layoutt';
 function Home(props) {
   return (
    <Layoutt>
-          <h1>Home</h1>
+          <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet porro ad incidunt suscipit dolorem, eaque, provident expedita itaque repellat consequatur corporis! Vel eum nostrum ab aspernatur, at cum sunt dignissimos suscipit tenetur, possimus aut laudantium ad doloribus voluptatem mollitia iste sed a ut expedita neque aliquid reiciendis. Tempore repudiandae distinctio aut consectetur eos ea unde eaque quos vero quia provident sint sed ipsum possimus sunt blanditiis doloribus veritatis esse, officia molestiae? Aut reprehenderit eveniet, non veniam dolores excepturi ipsum earum, dignissimos inventore expedita facilis nesciunt placeat totam hic in ratione. Dolorum, facilis eius at nostrum aut ex odio dignissimos mollitia!</h1>
 
    </Layoutt>
   );
